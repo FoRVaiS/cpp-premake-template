addfe5b0156ad886f12dcf6391c9d24f23d766d1
Benjamin Chiang <chiangbenjamin991@gmail.com>
2025-02-20 22:36:53 -0800

    feat: automatically set workspace name to dir name

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,23 @@
-workspace "cpp-workspace"
+function get_path_tail(path)
+  local tail = ""
+  local separator = package.config:sub(1, 1) == "\\" and "\\" or "/"
+  local pos = string.find(path, separator, nil, true)
+
+  if pos then
+    local last_pos = pos
+    while pos do
+      last_pos = pos
+      pos = string.find(path, separator, pos + 1, true)
+    end
+    tail = string.sub(path, last_pos + 1)
+  else
+    tail = path
+  end
+
+  return tail
+end
+
+workspace(get_path_tail(_WORKING_DIR))
   architecture "x86_64"
   configurations { "Debug", "Release", "Dist" }
 
